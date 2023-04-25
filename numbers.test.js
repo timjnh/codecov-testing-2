474a0efdd8fcfe5527582794031d41a103d55e77
Tim_Jones <tjones@aurorasolar.com>
2023-04-25 09:15:43 -0400

    Fixing the other tests...

--- numbers.test.js
+++ numbers.test.js
@@ -1,4 +1,4 @@
-import { addNumbers } from "./numbers.js";
+import { addNumbers, subtractNumbers, multiplyNumbers } from "./numbers.js";
 
 describe('addNumbers', () => {
   test("it should add two numbers together", () => {
